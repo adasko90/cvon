82a0bb6867ec3b119eb6af6970244df2f0beae67
adasko90 <adasko90@gmail.com>
2014-11-16 21:32:40 +0100

    Added new fields to users, some validations for users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+        validates :name, format: /[A-Z][a-z]*/
+       # validates :role, inclusion: { in: %w(admin user company)}
+        validates :name, :surname, :role, :gender, presence: true
 end
