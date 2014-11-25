86b1e8dae111af1fe9694b939ddb43dcb7c56532
adasko90 <adasko90@gmail.com>
2014-11-25 13:25:53 +0100

    Added category to companies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124211900) do
+ActiveRecord::Schema.define(version: 20141125115655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141124211900) do
     t.string  "company_name"
     t.string  "company_description"
     t.integer "user_id"
+    t.string  "category"
   end
 
   create_table "users", force: true do |t|
