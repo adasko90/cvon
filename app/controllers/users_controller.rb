72f6bddb8765b3eb51324fab3c2172bb03d72e3a
adasko90 <adasko90@gmail.com>
2014-11-30 20:05:29 +0100

    Trying to do companies registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
 
-  def show
-    @user=User.find(params[:id])
-  end
+def show
+  @user=User.find(params[:id])
+end
 
+
+
+private
   def user_params
     params.require(:user).permit(:name, :surname, :email, :password, :password_confirmation, :telephone, :role, :gender)
   end
